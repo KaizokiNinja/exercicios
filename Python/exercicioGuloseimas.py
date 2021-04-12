e721030fb4a4a7d9153a79d9012e1880ec7ac214
Jackson Wagner <jackson.anime@gmail.com>
2021-04-12 20:58:26 -0300

    master

---
+++ Python/exercicioGuloseimas.py
@@ -0,0 +1,11 @@
+class Guloseima :
+    def __init__(self):
+        self._sabor = ""
+    def getSabor(self):
+        return self._sabor
+    def setSabor (self, sabor):
+        self._sabor = sabor
+class bala(Guloseima):
+    pass
+class Bombom :
+    def __init__(self):
