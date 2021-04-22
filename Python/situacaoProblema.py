e56f935e3584b7f52402c787783be7ae8dbf9c9b
Jackson Wagner <jackson.anime@gmail.com>
2021-04-22 20:19:54 -0300

    master

---
+++ Python/situacaoProblema.py
@@ -0,0 +1,14 @@
+class pedido 
+class salgadinho :
+    def __init__(self,tipo,massa,molho):
+        self.__tipo = tipo
+        self.__massa = massa
+        self.__molho = molho
+
+
+            
+class itensVendidos :
+    def __init__(self) :
+        self.__precoDeVenda = ""
+        self.__dataDeValidade = time.time
+        self.__peso = 0
