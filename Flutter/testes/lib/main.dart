d3155f369b9fa1b95c362ac99290804bca9a53f8
Jackson Wagner <jackson.anime@gmail.com>
2021-05-10 22:17:19 -0300

    master

---
+++ Flutter/testes/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Teste'),
+        ),
+        body: GridView.count(
+          crossAxisCount: 3,
+          scrollDirection: Axis.vertical,
+          children: [
+            Container(
+              height: 10,
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+            Container(
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
